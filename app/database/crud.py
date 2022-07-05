9fbed61bba4e129528bfbd634e9c776c7755388c
Anthony Cepeda <72019805+anthonycepeda@users.noreply.github.com>
2022-07-05 07:16:22 +0200

    database: session param and model improvements

--- app/database/crud.py
+++ app/database/crud.py
@@ -4,8 +4,7 @@ from app.database import create_tables, get_session
 from app.database.models import User, UserCreate, UserRead
 
 
-def create_user(user: UserCreate):
-    db = get_session()
+def create_user(user: UserCreate, db: Session):
     user_to_db = User.from_orm(user)
     db.add(user_to_db)
     db.commit()
@@ -13,14 +12,13 @@ def create_user(user: UserCreate):
     return user_to_db
 
 
-def read_users():
-    db = get_session()
+def read_users(db: Session):
     users = db.exec(select(User)).all()
+    users = [user.dict() for user in users]
     return users
 
 
-def read_user(user_id: int):
-    db = get_session()
+def read_user(user_id: int, db: Session):
     user = db.get(User, user_id)
     if user:
         return user
--- app/database/models.py
+++ app/database/models.py
@@ -1,13 +1,24 @@
-from typing import Optional
-
-from pydantic.types import Optional
+from pydantic.types import Optional, Enum
 from sqlmodel import Field, SQLModel
+
+
+class Cities(str, Enum):
+    madrid = "madrid"
+    roma = "roma"
+    london = "london"
+    paris = "paris"
+    alicante = "alicante"
+    lisbon = "lisbon"
+    santo_domingo = "santo domingo"
 
 
 class UserBase(SQLModel):
     name: Optional[str]
     email: Optional[str]
     profession: Optional[str]
+    city: Optional[Cities]
+    vehicule: Optional[bool]
+    seniority: Optional[str]
 
     class Config:
         inherit_cache = False
